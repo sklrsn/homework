3353d8a909353451d1ed5f26d01d1c02d9c5fc14
kalai <kalaiarasan.saminathan@ssh.com>
2021-03-27 21:37:27 +0200

    Added new pkg

--- preprocessor/app/model.go
+++ preprocessor/app/model.go
@@ -1,4 +1,4 @@
-package main
+package app
 
 type Operation interface {
 	Read()
--- preprocessor/main.go
+++ preprocessor/main.go
@@ -1,15 +1,28 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+
+	"github.com/sklrsn/homework/preprocessor/app"
+)
 
 func main() {
+	fmt.Println("**************")
+	fmt.Println(os.Getenv("end_point"))
+	fmt.Println(os.Getenv("access_key_id"))
+	fmt.Println(os.Getenv("secret_access_key"))
+	fmt.Println(os.Getenv("region"))
+	fmt.Println("**************")
 	endpoint := os.Getenv("end_point")
-	creds := Credentials{
+	creds := app.Credentials{
 		AccessKey:       os.Getenv("access_key_id"),
 		SecretAccessKey: os.Getenv("secret_access_key"),
 		Region:          os.Getenv("region"),
 		EndPoint:        &endpoint,
 	}
-	app := new(App)
+	app := new(app.App)
 	app.Init(creds)
+	fmt.Println(app.SQSClient)
+	fmt.Println(app.KinesisClient)
 }
